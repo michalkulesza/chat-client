fa41babcb99584ccf2ed1ac7baf302a9a4836467
unknown <kulesza.michal@gmail.com>
2020-05-26 19:34:46 +0200

    scroll to bottom of the chat

--- src/components/Messages/Messages.tsx
+++ src/components/Messages/Messages.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./Messages.scss";
+import ScrollToBottom from "react-scroll-to-bottom";
 
-import Message from "../Message/Message";
+import Message from "./Message/Message";
 
 interface Props {
 	messages: { text: string; name: string }[];
@@ -11,25 +12,27 @@ interface Props {
 const Messages: React.FC<Props> = ({ messages, name }) => {
 	return (
 		<div className="messages">
-			{messages.map((message, i) => {
-				let type: string;
+			<ScrollToBottom className="messages-wrapper">
+				{messages.map((message, i) => {
+					let type: string;
 
-				if (message.name === "admin") {
-					type = "admin";
-				} else if (message.name === name) {
-					type = "user";
-				} else {
-					type = "partner";
-				}
+					if (message.name === "admin") {
+						type = "admin";
+					} else if (message.name === name) {
+						type = "user";
+					} else {
+						type = "partner";
+					}
 
-				return (
-					<Message
-						key={i * Math.random()}
-						content={message.text}
-						type={type}
-					></Message>
-				);
-			})}
+					return (
+						<Message
+							key={i * Math.random()}
+							content={message.text}
+							type={type}
+						></Message>
+					);
+				})}
+			</ScrollToBottom>
 		</div>
 	);
 };
