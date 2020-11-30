cfa80ac407a06a0395ea2fdf8b78a2e4a733105c
Michal <kulesza.michal@gmail.com>
2020-11-30 11:32:18 +0100

    remove: socket encryption

--- src/socketio/index.tsx
+++ src/socketio/index.tsx
@@ -1,9 +1,6 @@
 import io from "socket.io-client";
-import encrypt from "socket.io-encrypt";
-
 import PATH from "../constants/path";
 
 const socket = io(PATH);
-encrypt("someStrongSecret")(socket);
 
 export default socket;
